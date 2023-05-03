eadd86498b7904c395a2250535e6d8f8b345338e
João Pedro <jpedro1580@gmail.com>
2023-05-02 23:39:07 -0300

    Fix syntax errors

--- kotlin/credit-me/src/main/kotlin/app/creditme/domain/Credit.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/domain/Credit.kt
@@ -8,9 +8,9 @@ import app.creditme.enummeration.Status
 data class Credit(
     val creditCode: UUID = UUID.randomUUID(),
     val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFisrtInstallment: LocalDate
+    val dayFisrtInstallment: LocalDate,
     val numberOfInstallments: Int  = 0,
     val status: Status = Status.IN_PROGRESS,
-    val customer: Customer?
+    val customer: Customer?,
     val id: Long? = null
 )
