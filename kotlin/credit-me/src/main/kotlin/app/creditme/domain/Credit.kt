f449da3678853b58b4e2f30c9304b93fa3178f0f
João Pedro <jpedro1580@gmail.com>
2023-05-08 19:21:15 -0300

    Fix Typo

--- kotlin/credit-me/src/main/kotlin/app/creditme/domain/Credit.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/domain/Credit.kt
@@ -20,7 +20,7 @@ import jakarta.persistence.Enumerated
 data class Credit(
     @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
     @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
-    @Column(nullable = false) val dayFisrtInstallment: LocalDate,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
     @Column(nullable = false) val numberOfInstallments: Int  = 0,
     @Enumerated val status: Status = Status.IN_PROGRESS,
     @ManyToOne  val customer: Customer? = null,
