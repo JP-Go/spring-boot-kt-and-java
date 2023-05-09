10b9b92815264b3fab44eef9a73334b50ee326fa
João Pedro <jpedro1580@gmail.com>
2023-05-08 22:02:15 -0300

    Implement missing methods on the credit repository

--- kotlin/credit-me/src/main/kotlin/app/creditme/repository/CreditRepository.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/repository/CreditRepository.kt
@@ -2,8 +2,16 @@ package app.creditme.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
+import org.springframework.data.jpa.repository.Query
 
 import app.creditme.domain.Credit
 
 @Repository
-interface CreditRepository: JpaRepository<Credit,Long>
+interface CreditRepository: JpaRepository<Credit,Long>{
+
+    fun findByCreditCode(creditCode: UUID):Credit?
+
+    @Query(value = "SELECT * FROM credit WHERE customer_id = ?1",nativeQuery= true)
+    fun findAllByCustomer(customerId:Long): List<Credit>
+}
