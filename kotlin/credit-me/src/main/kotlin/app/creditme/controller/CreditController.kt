266d52d45210631570db8cab5c4d36025a38c7b4
João Pedro <jpedro1580@gmail.com>
2023-05-09 23:34:59 -0300

    Refactor controller methods

--- kotlin/credit-me/src/main/kotlin/app/creditme/controller/CreditController.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/controller/CreditController.kt
@@ -4,14 +4,16 @@ import app.creditme.dto.CreditDto
 import app.creditme.dto.CreditView
 import app.creditme.dto.CreditViewList
 import app.creditme.service.impl.CreditService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.PathVariable
 import java.util.UUID
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/credit")
@@ -19,26 +21,31 @@ class CreditController(
     private val creditService: CreditService,
 ) {
 
-    @PostMapping
-    fun saveCredit(@RequestBody creditDto: CreditDto): String {
-        this.creditService.save(creditDto.toEntity()).run {
-            return "Credit ${this.creditCode} created for customer ${this.customer?.firstName}"
-        }
+  @PostMapping
+  fun saveCredit(@RequestBody creditDto: CreditDto): ResponseEntity<String> {
+    this.creditService.save(creditDto.toEntity()).run {
+      return ResponseEntity(
+          "Credit ${this.creditCode} created for customer ${this.customer?.firstName}",
+          HttpStatus.CREATED
+      )
     }
+  }
 
-    @GetMapping
-    fun findAllByCustomerId(@RequestParam(value = "customer") customerId: Long): List<CreditViewList> {
-        this.creditService.findAllByCustomer(customerId)
-            .map { CreditViewList(it) }
-            .also { return it }
+  @GetMapping
+  fun findAllByCustomerId(
+      @RequestParam(value = "customer") customerId: Long
+  ): ResponseEntity<List<CreditViewList>> {
+    this.creditService.findAllByCustomer(customerId).map { CreditViewList(it) }.also {
+      return ResponseEntity(it,HttpStatus.OK)
     }
+  }
 
-    @GetMapping("/{creditCode}")
-    fun findByCreditCode(
-        @RequestParam("customer") customerId: Long,
-        @PathVariable("creditCode") creditCode: UUID,
-    ): CreditView {
-        val credit = this.creditService.findByCreditCode(creditCode, customerId)
-        return CreditView(credit)
-    }
+  @GetMapping("/{creditCode}")
+  fun findByCreditCode(
+      @RequestParam("customer") customerId: Long,
+      @PathVariable("creditCode") creditCode: UUID,
+  ): ResponseEntity<CreditView> {
+    val credit = this.creditService.findByCreditCode(creditCode, customerId)
+    return ResponseEntity(CreditView(credit),HttpStatus.OK)
+  }
 }
--- kotlin/credit-me/src/main/kotlin/app/creditme/controller/CustomerController.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/controller/CustomerController.kt
@@ -1,52 +1,46 @@
 package app.creditme.controller
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PatchMapping
-
-import app.creditme.service.impl.CustomerService
-
 import app.creditme.dto.CustomerDto
 import app.creditme.dto.CustomerUpdateDto
 import app.creditme.dto.CustomerView
-
-
+import app.creditme.service.impl.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController()
 @RequestMapping("/api/v1/customer")
-class CustomerController(
-        private val customerService: CustomerService
-) {
+class CustomerController(private val customerService: CustomerService) {
 
-    @PostMapping
-    fun registerCustomer(@RequestBody customerDto: CustomerDto):String {
-        val customer = this.customerService.save(customerDto.toEntity())
-        return "Customer $customer.email saved"
-    }
+  @PostMapping
+  fun registerCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<String> {
+    val customer = this.customerService.save(customerDto.toEntity())
+    return ResponseEntity("Customer $customer.email saved", HttpStatus.CREATED)
+  }
 
-    @GetMapping("/{id}")
-    fun findCustomerById(@PathVariable id:Long): CustomerView {
-        val customer = this.customerService.findById(id)
-        return CustomerView(customer)
-    }
-    @DeleteMapping("/{id}")
-    fun deleteCustomer(@PathVariable id: Long) = this.customerService.deleteById(id)
+  @GetMapping("/{id}")
+  fun findCustomerById(@PathVariable id: Long): ResponseEntity<CustomerView> {
+    val customer = this.customerService.findById(id)
+    return ResponseEntity(CustomerView(customer), HttpStatus.OK)
+  }
+  @DeleteMapping("/{id}")
+  fun deleteCustomer(@PathVariable id: Long): ResponseEntity<Unit> =
+      ResponseEntity(this.customerService.deleteById(id), HttpStatus.NO_CONTENT)
 
-    @PatchMapping("/{id}")
-    fun updateCustomer(@PathVariable id: Long, customerUpdateDto: CustomerUpdateDto): CustomerView {
+  @PatchMapping("/{id}")
+  fun updateCustomer(@PathVariable id: Long, customerUpdateDto: CustomerUpdateDto): ResponseEntity<CustomerView> {
 
-        val customer = this.customerService.findById(id).apply {
-            customerUpdateDto.toEntity(this)
-        }
+    val customer = this.customerService.findById(id).apply { customerUpdateDto.toEntity(this) }
 
-        val updatedCustomer = this.customerService.save(customer)
+    val updatedCustomer = this.customerService.save(customer)
 
-        return CustomerView(updatedCustomer)
-    }
-
+    return ResponseEntity(CustomerView(updatedCustomer),HttpStatus.OK)
+  }
 }
