bb98ca16ca8324f2c7dfb2f3cac9217d4787f5ef
João Pedro <jpedro1580@gmail.com>
2023-05-11 22:52:09 -0300

    Implement field validation on DTOs

--- kotlin/credit-me/src/main/kotlin/app/creditme/dto/CreditDto.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/dto/CreditDto.kt
@@ -2,19 +2,30 @@ package app.creditme.dto
 
 import app.creditme.domain.Credit
 import app.creditme.domain.Customer
+import app.creditme.validation.ValidDayOfInstallment
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
+    
+    @field:NotNull(message = "Credit Value missing")
+    @field:Positive(message = "Credit value must be positive")
     val creditValue: BigDecimal,
+    @field:NotNull(message = "Day of first installment missing")
+    @field:ValidDayOfInstallment(message = "Day of first installment must be after today and before 90 days from today")
     val dayFirstInstallment: LocalDate,
+    @field:NotNull(message = "Number of installments missing")
     val numberOfInstallments: Int,
+    @field:NotNull(message = "Customer Id missing")
     val customerId: Long
-){
-    fun toEntity(): Credit = Credit(
-        creditValue = this.creditValue,
-        dayFirstInstallment = this.dayFirstInstallment,
-        numberOfInstallments = this.numberOfInstallments,
-        customer = Customer(id = this.customerId)
-    )
+) {
+  fun toEntity(): Credit =
+      Credit(
+          creditValue = this.creditValue,
+          dayFirstInstallment = this.dayFirstInstallment,
+          numberOfInstallments = this.numberOfInstallments,
+          customer = Customer(id = this.customerId)
+      )
 }
--- kotlin/credit-me/src/main/kotlin/app/creditme/dto/CustomerDto.kt
+++ kotlin/credit-me/src/main/kotlin/app/creditme/dto/CustomerDto.kt
@@ -3,16 +3,34 @@ package app.creditme.dto
 import java.math.BigDecimal
 import app.creditme.domain.Customer
 import app.creditme.domain.Address
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Digits
+import jakarta.validation.constraints.PositiveOrZero
+import org.hibernate.validator.constraints.br.CPF
 
 data class CustomerDto(
-  val firstName: String,
-  val lastName: String,
-  val cpf: String,
-  val income: BigDecimal,
-  val email: String,
-  val password: String,
-  val zipCode: String,
-  val street: String
+   @field:NotEmpty(message = "FirstName missing") val firstName: String,
+   @field:NotEmpty(message = "LastName missing") val lastName: String,
+   @field:NotEmpty(message = "CPF missing") 
+   @field:CPF(message = "Invalid CPF")
+   val cpf: String,
+   @field:NotEmpty(message = "Email missing") 
+   @field:Email(message = "Invalid email")
+   val email: String,
+   @field:NotEmpty(message  = "Password missing") 
+   @field:Min(6, message = "Password must have 6 characters or more")
+   val password: String,
+   @field:NotEmpty(message = "ZipCode missing")
+   @field:Digits(message = "ZipCode must contain only digits", integer = 7,fraction = 0)
+   val zipCode: String,
+   @field:NotEmpty(message = "Street missing")
+   val street: String,
+   @field:NotNull(message = "Missing income")
+   @field:PositiveOrZero(message = "Income must be positive or zero")
+   val income: BigDecimal,
 ) {
   
  fun toEntity(): Customer = Customer(
